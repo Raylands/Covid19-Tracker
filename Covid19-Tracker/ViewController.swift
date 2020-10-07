820e6dfd73ed790154e5059cb5b1b7ed881d9fa5
Marco Exner <maexn20@student.sdu.dk>
2020-10-07 12:35:56 +0200

    Initial Commit

---
+++ Covid19-Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Covid19-Tracker
+//
+//  Created by Marco Exner on 07.10.20.
+//  Copyright © 2020 Marco Exner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
